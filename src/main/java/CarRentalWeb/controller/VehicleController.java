66fc4e8da3acae8aab4b551fe39af0319d3a7e3e
Jesper <88264190+SplinteeX@users.noreply.github.com>
2024-09-17 14:51:27 +0300

    Fixed the fixings

--- src/main/java/CarRentalWeb/controller/VehicleController.java
+++ src/main/java/CarRentalWeb/controller/VehicleController.java
@@ -1,4 +1,33 @@
 package CarRentalWeb.controller;
 
+import CarRentalWeb.model.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/vehicles")
 public class VehicleController {
+
+    private final MongoTemplate mongoTemplate;
+
+    @Autowired
+    public VehicleController(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @PostMapping("/add")
+    public void addVehicle(@RequestBody Vehicle vehicle) {
+        mongoTemplate.insert(vehicle);
+        System.out.println("Vehicle added: " + vehicle.getVehicleId());
+    }
+
+    @PostMapping("/addMultiple")
+    public void addMultipleVehicles(@RequestBody List<Vehicle> vehicles) {
+        mongoTemplate.insertAll(vehicles);
+        System.out.println(vehicles.size() + " vehicles added.");
+    }
 }
--- src/main/java/CarRentalWeb/model/Vehicle.java
+++ src/main/java/CarRentalWeb/model/Vehicle.java
@@ -1,4 +1,37 @@
 package CarRentalWeb.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "vehicles")
 public class Vehicle {
+    @Id
+    private String vehicleId;
+    private String vehicleReg;
+    private boolean vehicleAvailable;
+    private String vehicleColor;
+    private String vehicleType;
+    private double vehiclePrice;
+    private String vehicleModel;
+    private String vehicleMF;
+    private String vehicleTrans;
+
+    public String getVehicleId() { return vehicleId; }
+    public void setVehicleId(String vehicleId) { this.vehicleId = vehicleId; }
+    public String getVehicleReg() { return vehicleReg; }
+    public void setVehicleReg(String vehicleReg) { this.vehicleReg = vehicleReg; }
+    public boolean isVehicleAvailable() { return vehicleAvailable; }
+    public void setVehicleAvailable(boolean vehicleAvailable) { this.vehicleAvailable = vehicleAvailable; }
+    public String getVehicleColor() { return vehicleColor; }
+    public void setVehicleColor(String vehicleColor) { this.vehicleColor = vehicleColor; }
+    public String getVehicleType() { return vehicleType; }
+    public void setVehicleType(String vehicleType) { this.vehicleType = vehicleType; }
+    public double getVehiclePrice() { return vehiclePrice; }
+    public void setVehiclePrice(double vehiclePrice) { this.vehiclePrice = vehiclePrice; }
+    public String getVehicleModel() { return vehicleModel; }
+    public void setVehicleModel(String vehicleModel) { this.vehicleModel = vehicleModel; }
+    public String getVehicleMF() { return vehicleMF; }
+    public void setVehicleMF(String vehicleMF) { this.vehicleMF = vehicleMF; }
+    public String getVehicleTrans() { return vehicleTrans; }
+    public void setVehicleTrans(String vehicleTrans) { this.vehicleTrans = vehicleTrans; }
 }
